83d3d7bcb12382cb50ba3c25f7e548ba6606984e
b4b4r07 <b4b4r07@gmail.com>
2017-05-08 02:49:32 +0900

    id conv

--- cli/screen.go
+++ cli/screen.go
@@ -14,7 +14,7 @@ import (
 
 type Screen struct {
 	Text  string
-	IDs   map[string]string
+	ID    func(string) string
 	Pages *crowi.Pages
 }
 
@@ -47,16 +47,24 @@ func NewScreen() (*Screen, error) {
 		return &Screen{}, errors.New(res.Error)
 	}
 
+	// TODO: map ?
 	text := ""
-	ids := make(map[string]string, len(res.Pages))
-	for _, page := range res.Pages {
-		text += fmt.Sprintf("%s\n", page.Path)
-		ids[page.Path] = page.ID
+	for _, pi := range res.Pages {
+		text += fmt.Sprintf("%s\n", pi.Path)
+	}
+
+	id := func(path string) (id string) {
+		for _, pi := range res.Pages {
+			if pi.Path == path {
+				return pi.ID
+			}
+		}
+		return ""
 	}
 
 	return &Screen{
 		Text:  text,
-		IDs:   ids,
+		ID:    id,
 		Pages: res,
 	}, nil
 }
@@ -81,15 +89,15 @@ func (s *Screen) Filter() (selectedLines []string, err error) {
 type Line struct {
 	Path      string
 	URL       string
-	LocalPath string
 	ID        string
+	LocalPath string
 }
 
 func (s *Screen) ParseLine(line string) (*Line, error) {
 	return &Line{
 		Path:      line,
 		URL:       path.Join(Conf.Crowi.BaseURL, line),
-		LocalPath: filepath.Join(Conf.Crowi.LocalPath, s.IDs[line]),
-		ID:        s.IDs[line],
+		ID:        s.ID(line),
+		LocalPath: filepath.Join(Conf.Crowi.LocalPath, s.ID(line)),
 	}, nil
 }
